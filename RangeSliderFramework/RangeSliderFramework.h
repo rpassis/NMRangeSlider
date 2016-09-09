28aed3a449186bafb690ad066fa2c8c48d2db297
Rogerio de Paula Assis <rogerio@freshmob.com.au>
2016-09-09 14:47:15 +1000

    More tests

--- RangeSliderFramework/RangeSliderFramework.h
+++ RangeSliderFramework/RangeSliderFramework.h
@@ -16,6 +16,6 @@ FOUNDATION_EXPORT const unsigned char RangeSliderFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RangeSliderFramework/PublicHeader.h>
 
-#import "NMRangeSlider.h"
+#import <RangeSliderFramework/NMRangeSlider.h>
 
 
